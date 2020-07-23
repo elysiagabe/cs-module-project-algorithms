933b42388abe91cafd07f9a0658e91ace1392296
Elysia Gabe <elysiagabe@gmail.com>
2020-07-22 18:59:34 -0700

    Completes first pass solution for sliding_window_max

--- sliding_window_max/sliding_window_max.py
+++ sliding_window_max/sliding_window_max.py
@@ -1,11 +1,26 @@
 '''
 Input: a List of integers as well as an integer `k` representing the size of the sliding window
 Returns: a List of integers
+
+Notes: 
+-will need to iterate thru array
+-need to look at slices of arr that have length k...
+-only need to iterate thru to index len(arr)-k+1
+-when looking at a slice, find the max value and add that to an array with the other max values
 '''
 def sliding_window_max(nums, k):
     # Your code here
-
-    pass
+    max_values = []
+    # Iterate thru array
+    for i in range(0, len(nums)-k+1):
+        # these will be the numbers used to look at a slice of the array
+        start, end = i, i+k
+        # find the window max 
+        window_max = max(nums[start:end])
+        # add max to max_values arr
+        max_values.append(window_max)
+    # return arr
+    return max_values
 
 
 if __name__ == '__main__':
